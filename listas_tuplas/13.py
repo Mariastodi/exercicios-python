16bd8cba39b56588d6a426a94584ffc6f62ee6eb
Maria Beatriz <mariabeatrizcomputador2.0@gmail.com>
2026-02-05 12:54:46 -0300

    13.py

---
+++ listas_tuplas/13.py
@@ -0,0 +1,5 @@
+numeros_lc = [1, 2, 3, 4, 5]
+
+quadrados_comprehension = [n ** 2 for n in numeros_lc]
+print(f"Lista original: {numeros_lc}")
+print(f"Com list comprehension: {quadrados_comprehension}")
