ac6a3b5d43e18516bab48ab78b1b88803c8fc245
Maria Beatriz <mariabeatrizcomputador2.0@gmail.com>
2026-02-05 12:56:31 -0300

    15.py

---
+++ listas_tuplas/15.py
@@ -0,0 +1,4 @@
+numeros_fatiamento = [0, 1, 2, 3, 4, 5]
+print(f"Lista original antes da substituição: {numeros_fatiamento}")
+numeros_fatiamento[2:5] = [20, 30, 40]
+print(f"Lista após numeros[2:5] = [20, 30, 40]: {numeros_fatiamento}")
