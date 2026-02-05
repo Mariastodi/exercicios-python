5c004e0d1ae45674b17d9a716327bac1a036c71a
Maria Beatriz <mariabeatrizcomputador2.0@gmail.com>
2026-02-05 16:26:59 -0300

    24.py

---
+++ listas_tuplas/24.py
@@ -0,0 +1,9 @@
+carrinho = ['Mouse', 'Teclado', 'Monitor']
+
+print(f"Item 1: {carrinho[0]}")
+
+carrinho[1] = 'Headset Gamer'
+
+carrinho.append('Webcam')
+
+print(f"Carrinho Atualizado: {carrinho}")
