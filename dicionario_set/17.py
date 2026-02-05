95806ffbf69df14b635803c57fa32486eb81b7cb
Maria Beatriz <mariabeatrizcomputador2.0@gmail.com>
2026-02-05 15:57:53 -0300

    17.py

---
+++ dicionario_set/17.py
@@ -0,0 +1,5 @@
+produto = {"Nome": "celular", "Quantidade": 150, "Preço": 1900} 
+print(f"Dicionário do Produto: {produto}")
+
+aluno = {"nome": "Maria", "nota": 9.5} 
+print(f"Dicionário inicial: {aluno}")
