6da0e73712194a9bed4cfc6683bf11196cb50820
Maria Beatriz <mariabeatrizcomputador2.0@gmail.com>
2026-02-05 15:58:21 -0300

    18.py

---
+++ dicionario_set/18.py
@@ -0,0 +1,8 @@
+produto = {"Nome": "celular", "Quantidade": 150, "Preço": 1900} 
+print(f"Dicionário do Produto: {produto}")
+
+aluno = {"nome": "Maria", "nota": 9.5} 
+print(f"Dicionário inicial: {aluno}")
+
+aluno["curso"] = "Informática" 
+print(f"Dicionário após adicionar 'curso': {aluno}") 
