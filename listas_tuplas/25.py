20a64112e476ed028e0e39b3cd63cf34172f62f9
Maria Beatriz <mariabeatrizcomputador2.0@gmail.com>
2026-02-05 16:28:18 -0300

    25.py

---
+++ listas_tuplas/25.py
@@ -0,0 +1,8 @@
+coordenadas = (10, 20)
+ 
+print(f"Coordenada X: {coordenadas[0]}") 
+
+# TENTATIVA DE MODIFICAÇÃO (resulta em erro)
+coordenadas[0] = 5  
+ 
+print(f"Tupla Original: {coordenadas}")
