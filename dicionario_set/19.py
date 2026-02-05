48795b90845a485dfccda4452d2e8bd7da4622a5
Maria Beatriz <mariabeatrizcomputador2.0@gmail.com>
2026-02-05 16:29:16 -0300

    19.py

---
+++ dicionario_set/19.py
@@ -0,0 +1,13 @@
+usuario = {
+     'nome': 'Alice',
+     'idade': 30,
+     'email': 'alice@exemplo.com'
+}
+ 
+print(f"Nome do Usuário: {usuario['nome']}")
+ 
+usuario['idade'] = 31
+ 
+usuario['status'] = 'Ativo'
+ 
+print(f"Dicionário Atualizado: {usuario}")
